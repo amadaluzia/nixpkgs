8177ab1a5dfb476f4a1e9ba5952f13c46a9e996e
Artur Manuel <amad@atl.tools>
2025-03-18 01:20:27 +0000

    emojis: init at 0.7.0

---
+++ pkgs/development/python-modules/emojis/default.nix
@@ -0,0 +1,43 @@
+{
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  pythonOlder,
+  pandoc,
+  lib,
+}:
+let
+  version = "0.7.0";
+in
+buildPythonPackage {
+  pname = "emojis";
+  inherit version;
+  pyproject = true;
+
+  build-system = [ setuptools ];
+
+  disabled = pythonOlder "3.4";
+
+  src = fetchFromGitHub {
+    owner = "alexandrevicenzi";
+    repo = "emojis";
+    tag = "v${version}";
+    hash = "sha256-rr/BM39U1j8EL8b/YojclI4h0NnOCdoMlecR/1f9ISg=";
+  };
+
+  nativeBuildInputs = [
+    pandoc
+  ];
+
+  preBuild = ''
+    make pandoc
+  '';
+
+  meta = {
+    description = "Emojis for Python";
+    homepage = "https://github.com/alexandrevicenzi/emojis";
+    changelog = "https://github.com/alexandrevicenzi/emojis/releases/tag/v${version}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ amadaluzia ];
+  };
+}
